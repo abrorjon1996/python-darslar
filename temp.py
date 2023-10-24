b68fa954e74e1ca8be140774fec76049d3dbeb7a
abrorjon1996 <147814339+abrorjon1996@users.noreply.github.com>
2023-10-24 12:16:14 +0500

    Update temp.py

--- temp.py
+++ temp.py
@@ -1,8 +1,38 @@
 21/11/2023
-Dasturlash Asoslari
-12 dars: xatolar bilan ishlash
- mualif: Asqarov Abror
-  
-   print mevalar("olm","anor","uzum","anjir")
- dastur tugadi
+#Dasturlash Asoslari
+#14 dars: Dictionary (lug'at')
+# mualif: Asqarov Abror
  
+#car_0 ={ 'model':'ferari','rang':'qizil'}
+#print(car_0['model'])
+#print(car_0['rang'])
+
+#en_uz = {'apple':'olma','apricot':"o'rik",'banana':"banan"}#mevalar = {'olma':1000,'tarvuz':8000,'qovun':1000}
+#print(f"olma narhi {mevalar['olma']} so'm")
+
+#talaba_0 ={'ism':'murod olimov','yosh':20,'t_yil':2000}
+#print(f"{talaba_0['ism'].title()},\
+    #  {talaba_0['t_yil']}-yilda tig'ilgan,\
+    #  {talaba_0['yosh']} yoshda")   
+
+#yangi kalit qoshish
+#talaba_0['kurs'] = 4
+#talaba_0['fakultet'] ='informatika'
+#talaba_0['ism'] = 'abdulloh'
+##bow lugat} {talaba_1[kurs]}-kurs")
+#@kalit so'z nqiymatni o'chirib tashlash
+#talaba_0 = {'ism':'alijon olimov','yosh':20,'t_yil':2000}
+#print(talaba_0)
+#del talaba_0['yosh']
+#print(talaba_0)
+
+#lug'atni bir nechta qatorda yozish
+telefonlar = {
+    'ali':'iphone x',
+    'aziz':'galaxy s9',
+    'akmal':'mi 10 pro',
+    'xumor':'nokia 6700'
+    }
+phone = telefonlar.get('hasan')
+ptint(phone)
+     
