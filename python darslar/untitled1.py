8ecaec32bc76439bdb1b791b29eed067535ea314
abrorjon1996 <147814339+abrorjon1996@users.noreply.github.com>
2023-10-28 12:42:39 +0500

    Create untitled1.py

---
+++ python darslar/untitled1.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct 28 10:57:21 2023
+
+@author: User
+"""#15 dars lugatlar bilan ishlash
+#'fakultet':'fizika',
+   # 'kurs':3
+  #  }
+
+#@print(talaba_0.ite/ms())
+ #    'olma':10000,
+#     #'anor':2000,
+     #'olcha':23000
+    #}
+#
+#print(mahsulotlar.keys())
+  
+#print('do\'kondagi mahsulotlar:')
+#for mahsulot in mahsulotlar.keys():
+ #   print(mahsulot.title())
+ 
+#print('do\'kondagi mahsulotlar:')
+#for mahsulot in mahsulotlar:
+#    print(mahsulot.title())
+#bozorlik = ['anor','uzum','non','baliq']
+#for mahsulot in mahsulotlar:
+   # if mahsulot in mahsulot:
+  #      print(f"{mahsulot.title()} {mahsulotlar[mahsulot]} so'm")
+#@for buyum in bozorlik:
+#    if buyum not in mahsulotlar:
+       # print(f"iltimos,do'koningizga {buyum} ham olib keling")
+         
+        #lug'at elemntlarini tartib bilan chiqarish 
+#print("do'konimizdagi mahsulotlar:")
+#for mahsulot in sorted(mahsulotlar):
+   #  print(mahsulot.title())
+telefonlar = {
+    'ali':'iphone 10x',
+    'sanjar':'iphone 10x',
+    'vali':'samsung',
+    'jahongir':'iphone 10x',
+    'salim':'gelxy a 12'
+    }   
+print('foydalanuvchilar quyidagi telefonlarni ishlatishadi:')
+for tel in telefonlar.values():
+    print(tel)
+    #set
+print('foydalanuvchilar quyidagi telefonlarni ishlatishadi:') 
+for tel in set(telefonlar.values()):
+          print(tel)
+
+toys = {"ball","car","lamp","ball"}
+
